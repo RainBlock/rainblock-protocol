37809f28fcbb369397a62327da1b2e89a60e07ad
Michael Wei <mwei@cs.ucsd.edu>
2019-04-04 14:25:30 -0700

    :boom: minimize verifier-storage overhead

--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,7 @@
 import { VerifierService, VerifierClient, IVerifierServer, IVerifierClient } from '../generated/clientVerifier_grpc_pb'
 import { TransactionRequest, TransactionReply, ErrorCode } from '../generated/clientVerifier_pb';
 import { VerifierStorageService, VerifierStorageClient, IVerifierStorageClient, IVerifierStorageService, IVerifierStorageServer } from '../generated/verifierStorage_grpc_pb'
-import { UpdateMsg, UpdateOp, ValueChangeOp, DeletionOp, CreationOp, ExecutionOp, StorageDeletion, StorageInsertion, StorageUpdate } from '../generated/verifierStorage_pb';
+import { UpdateMsg, UpdateOp, StorageUpdate } from '../generated/verifierStorage_pb';
 import { StorageNodeClient, StorageNodeService, IStorageNodeClient, IStorageNodeServer } from '../generated/clientStorage_grpc_pb';
 import { MerklePatriciaTreeNode, RPCWitness, CodeRequest, CodeReply, AccountRequest, AccountReply, StorageRequest, StorageReply, BlockHashReply, BlockHashRequest } from '../generated/clientStorage_pb';
 
@@ -11,8 +11,7 @@ import * as grpc from 'grpc';
 
 export { VerifierService, VerifierClient, TransactionRequest, TransactionReply, IVerifierClient, 
     IVerifierServer, ErrorCode, VerifierStorageService, VerifierStorageClient, IVerifierStorageClient, 
-    IVerifierStorageServer, UpdateMsg, UpdateOp, ValueChangeOp, DeletionOp, CreationOp, ExecutionOp, 
-    StorageDeletion, StorageInsertion, StorageUpdate,
+    IVerifierStorageServer, UpdateMsg, UpdateOp, StorageUpdate,
     StorageNodeClient, StorageNodeService, IStorageNodeClient, IStorageNodeServer,
     MerklePatriciaTreeNode, RPCWitness, CodeRequest, CodeReply, AccountRequest, AccountReply, 
     StorageRequest, StorageReply, BlockHashReply, BlockHashRequest,
