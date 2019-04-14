299f14eaf43456dce123c329dcda74806cc23d38
Amy Tai <amy.tai.2009@gmail.com>
2019-04-14 10:11:41 -0700

    Enable getAccounts to request for multiple addresses

--- ts/index.ts
+++ ts/index.ts
@@ -3,7 +3,7 @@ import { MerkleNodeAdvertisement, BlockAdvertisement, NeighborAdvertisement, Ver
 import { VerifierStorageService, VerifierStorageClient, IVerifierStorageClient, IVerifierStorageService, IVerifierStorageServer } from '../generated/verifierStorage_grpc_pb'
 import { UpdateMsg, UpdateOp, StorageUpdate } from '../generated/verifierStorage_pb';
 import { StorageNodeClient, StorageNodeService, IStorageNodeClient, IStorageNodeServer } from '../generated/clientStorage_grpc_pb';
-import { RPCWitness, CodeRequest, CodeReply, AccountRequest, AccountReply, StorageRequest, StorageReply, BlockHashReply, BlockHashRequest } from '../generated/clientStorage_pb';
+import { RPCWitness, CodeRequest, CodeReply, AccountsRequest, AccountReply, AccountsReply, StorageRequest, StorageReply, BlockHashReply, BlockHashRequest } from '../generated/clientStorage_pb';
 
 import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
 import * as grpc from 'grpc';
@@ -13,7 +13,7 @@ export { VerifierService, VerifierClient, TransactionRequest, TransactionReply, 
     IVerifierServer, ErrorCode, VerifierStorageService, VerifierStorageClient, IVerifierStorageClient, 
     IVerifierStorageServer, UpdateMsg, UpdateOp, StorageUpdate,
     StorageNodeClient, StorageNodeService, IStorageNodeClient, IStorageNodeServer,
-    RPCWitness, CodeRequest, CodeReply, AccountRequest, AccountReply, 
+    RPCWitness, CodeRequest, CodeReply, AccountsRequest, AccountReply, AccountsReply,
     MerkleNodeAdvertisement, BlockAdvertisement, NeighborAdvertisement, VerifierVerifierHandshakeMessage,
     StorageRequest, StorageReply, BlockHashReply, BlockHashRequest,
     IVerifierStorageService, google_protobuf_empty_pb, grpc };
